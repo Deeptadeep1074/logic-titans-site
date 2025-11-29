e400392f5e30069830c658f05ddb8ab745013e04
Deeptadeep Mitra <deeptadeepmitra708@gmail.com>
2025-11-29 19:20:35 +0530

    Add team photos and assets

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,4 +1,6 @@
 import React from "react";
+import DeeptadeepImg from "../assets/Deeptadeep.jpg";
+import DipyamanImg from "../assets/Dipyaman.jpg";
 
 function Team() {
   return (
@@ -6,17 +8,21 @@ function Team() {
       <h2>Meet Our Team</h2>
 
       <div className="team-members">
+
+        {/* Deeptadeep */}
         <div className="team-member member">
-          <img src="https://i.imgur.com/xa2F8dL.png" alt="Deeptadeep" />
+          <img src={DeeptadeepImg} alt="Deeptadeep" />
           <h3>Deeptadeep Mitra</h3>
           <p>Team Lead / AI Developer</p>
         </div>
 
+        {/* Dipyaman */}
         <div className="team-member member">
-          <img src="https://i.imgur.com/N6M1pnJ.png" alt="Dipyaman" />
+          <img src={DipyamanImg} alt="Dipyaman" />
           <h3>Dipyaman Bar</h3>
           <p>Co-Lead / FullStack Developer</p>
         </div>
+
       </div>
     </section>
   );
